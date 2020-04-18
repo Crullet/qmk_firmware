0e8e3e6a30f6eb62312d87cd2e6e0cd3fa27c284
Crullet <christiannessjogren@gmail.com>
2020-04-18 08:50:04 +0200

    Updated Keymap

--- keyboards/redox/keymaps/Crull/config.h
+++ keyboards/redox/keymaps/Crull/config.h
@@ -33,4 +33,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
 
-#define AUTO_SHIFT_TIMEOUT 150
+#define AUTO_SHIFT_TIMEOUT 105
+#define TAPPING_TERM 105
--- keyboards/redox/keymaps/Crull/rules.mk
+++ keyboards/redox/keymaps/Crull/rules.mk
@@ -1,3 +1,3 @@
 RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
 TAP_DANCE_ENABLE = no		# Tap Dance
-AUTO_SHIFT_ENABLE = yes 	# Long press becomes shifted key
+AUTO_SHIFT_ENABLE = no 	# Long press becomes shifted key
