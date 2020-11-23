dc54d76b350059f3ea88c31d5b0fc0f5f0a27b59
crullet <christiannessjogren@gmail.com>
2020-11-23 12:26:02 +0100

    Auto shift and tap dance enable

--- keyboards/planck/keymaps/Crullet/rules.mk
+++ keyboards/planck/keymaps/Crullet/rules.mk
@@ -1,7 +1,4 @@
 SRC += muse.c
-<<<<<<< HEAD
 ENCODER_ENABLE = yes
-=======
 TAP_DANCE_ENABLE = yes		# Tap Dance
 AUTO_SHIFT_ENABLE = yes 	# Long press becomes shifted key
->>>>>>> 8022a8f9f78eb6c71326e68bb3947d3b4f9a1fd4
