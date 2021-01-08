fb6ae7feb25af3425fbe5f77bdc68af207735058
crullet <christiannessjogren@gmail.com>
2021-01-08 14:18:32 +0100

    throubleshooting

--- keyboards/preonic/keymaps/Crullet/keymap.c
+++ keyboards/preonic/keymaps/Crullet/keymap.c
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    SE_ARNG,
   KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    SE_ODIA, SE_ADIA,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  SE_MINS, KC_ENT,
-  BACKLIT, SE_LABK, KC_LALT, KC_LGUI, LOW_BSP, KC_SPC,  KC_SPC,  RSE_DEL, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+  BACKLIT, SE_LABK, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Colemak
