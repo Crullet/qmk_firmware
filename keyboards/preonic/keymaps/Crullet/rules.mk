26333edc7a55e44285e7f6330aed98a3a89b0220
crullet <christiannessjogren@gmail.com>
2021-01-12 15:58:15 +0100

    Enable autoshift and disable Startup sound

--- keyboards/preonic/keymaps/Crullet/rules.mk
+++ keyboards/preonic/keymaps/Crullet/rules.mk
@@ -1,4 +1,4 @@
 SRC += muse.c
 ENCODER_ENABLE = yes
 TAP_DANCE_ENABLE = no		# Tap Dance
-AUTO_SHIFT_ENABLE = no 	# Long press becomes shifted key
+AUTO_SHIFT_ENABLE = yes 	# Long press becomes shifted key
